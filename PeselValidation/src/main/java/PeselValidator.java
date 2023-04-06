8ba83fa7a617212cd40bdc110ac8e08ac9212fcc
Paweł Wacławik <pawelwoe@gmail.com>
2023-04-06 10:39:13 +0200

    Second commit

--- PeselValidation/src/main/java/PeselValidator.java
+++ PeselValidation/src/main/java/PeselValidator.java
@@ -1,12 +1,6 @@
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.*;
 
 public class PeselValidator {
-
-    private static final Logger logger = LoggerFactory.getLogger(PeselValidator.class);
 
     public static void main(String[] args) {
         String fileName = args[0];
